24cc88b44cfa429891170cc91b1b17da8c686b9a
Joel Estumano <joelestumano@gmail.com>
2023-08-10 08:47:10 -0300

    hide endpoint swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiExcludeEndpoint()
   @Get()
   @Redirect('docs', 200)
   getHello(): string {
