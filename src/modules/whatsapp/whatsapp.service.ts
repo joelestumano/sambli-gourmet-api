b706f135dc4b8004ef06ddbaf82c1c48f7091fa2
Joel Estumano <joelestumano@gmail.com>
2023-08-17 16:48:19 -0300

    updates módulo whatsapp

--- src/modules/whatsapp/whatsapp.service.ts
+++ src/modules/whatsapp/whatsapp.service.ts
@@ -1,12 +1,12 @@
-import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { create, Message, Whatsapp } from 'venom-bot';
 import { OpenaiBotMessage, OpenaiService, WhatsappMessageType } from '../openai/openai.service';
 import { DtoWhatsappProfileName } from './dtos/whatsapp-profile-name.dto';
 import { DtoWhatsappProfileStatus } from './dtos/whatsapp-profile-status.dto';
 
-class Client {
+class WhatsappRefApi {
     constructor(private readonly whatsapp: Whatsapp) { }
-    getWhatsapp(): Whatsapp {
+    getInstance(): Whatsapp {
         return this.whatsapp;
     }
 }
@@ -14,51 +14,47 @@ class Client {
 @Injectable()
 export class WhatsappService {
 
-    private readonly logger = new Logger(WhatsappService.name);
-    private client: Client;
+    private whatsappRefApi: WhatsappRefApi;
 
     constructor(private readonly openaiService: OpenaiService) {
         create({
             session: 'session-sg-api',
             autoClose: 0,
         }).then(async (whatsapp: Whatsapp) => {
-            this.client = new Client(whatsapp);
-            return await this.start(this.client);
+            this.whatsappRefApi = new WhatsappRefApi(whatsapp);
+            return await this.start(this.whatsappRefApi);
         }).catch((error) => {
             throw new InternalServerErrorException(error);
         });
     }
 
-    private async start(client: Client): Promise<void> {
-        client.getWhatsapp().onMessage(async (message: Message) => {
+    private async start(whatsappRefApi: WhatsappRefApi): Promise<void> {
+        whatsappRefApi.getInstance().onMessage(async (message: Message) => {
             if (message.body && !message.isGroupMsg) {
 
                 this.openaiService.botMessage(message).then(async (botMessage: OpenaiBotMessage) => {
 
                     switch (botMessage.type) {
                         case WhatsappMessageType.text:
-                            return await client.getWhatsapp().sendText(message.chatId, `👱‍♀️ ${botMessage.message}`)
-                                .then((result: any) => {
-                                    this.logger.log(result.text);
-                                    return result;
-                                })
+                            return await whatsappRefApi.getInstance().sendText(message.chatId, `👱‍♀️ ${botMessage.response}`)
+                                .then((result: any) => result)
+                                .catch((error) => error);
+
+                        case WhatsappMessageType.reply:
+                            return await whatsappRefApi.getInstance().reply(message.chatId, `👱‍♀️ ${botMessage.response}`, message.id)
+                                .then((result) => result)
+                                .catch((error) => error);
+
+                        case WhatsappMessageType.image:
+                            return await whatsappRefApi.getInstance().sendImage(message.chatId, 'src\\temp\\banner.jpg', 'Banner', 'Caption')
+                                .then((result) => result)
+                                .catch((error) => error);
+
+                        case WhatsappMessageType.location:
+                            return await whatsappRefApi.getInstance().sendLocation(message.chatId, '-1.722247', '-48.879224', 'Location')
+                                .then((result) => result)
                                 .catch((error) => error);
                     }
-
-                    this.logger.log(message.body);
-
-                    /* return await client.getWhatsapp().reply(message.chatId, `👱‍♀️ ${content}`, message.id)
-                        .then((result) => result)
-                        .catch((error) => error); */
-
-                    /* return await client.getWhatsapp().sendImage(message.chatId, 'src\\temp\\banner.jpg', 'Banner', 'Caption')
-                        .then((result) => result)
-                        .catch((error) => error); */
-
-                    /* return await client.getWhatsapp().sendLocation(message.chatId, '-1.722247', '-48.879224', 'Location')
-                        .then((result) => result)
-                        .catch((error) => error); */
-
                 });
 
             }
@@ -66,25 +62,25 @@ export class WhatsappService {
     }
 
     async setProfileStatus(dto: DtoWhatsappProfileStatus): Promise<void> {
-        if (this.client) {
-            await this.client.getWhatsapp().setProfileStatus(dto.profileStatus);
+        if (this.whatsappRefApi) {
+            await this.whatsappRefApi.getInstance().setProfileStatus(dto.profileStatus);
         } else {
             throw new InternalServerErrorException('Whatsapp client is null');
         }
     }
 
     async setProfileName(dto: DtoWhatsappProfileName): Promise<void> {
-        if (this.client) {
-            await this.client.getWhatsapp().setProfileName(dto.profileName);
+        if (this.whatsappRefApi) {
+            await this.whatsappRefApi.getInstance().setProfileName(dto.profileName);
         } else {
             throw new InternalServerErrorException('Whatsapp client is null');
         }
     }
 
     async setProfilePic(file: Express.Multer.File): Promise<void> {
-        if (this.client) {
+        if (this.whatsappRefApi) {
             const filePath = `src/temp/${file.originalname}`;
-            await this.client.getWhatsapp().setProfilePic(filePath).then(() => {
+            await this.whatsappRefApi.getInstance().setProfilePic(filePath).then(() => {
                 const fs = require('fs');
                 fs.unlinkSync(filePath, (err: any) => {
                     if (err) {
