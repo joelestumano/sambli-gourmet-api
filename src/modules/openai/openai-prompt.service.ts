33988554c5da869ab5cdd88b8caf36eae5c58ccf
Joel Estumano <joelestumano@gmail.com>
2023-08-17 19:07:19 -0300

    updates

--- src/modules/openai/openai-prompt.service.ts
+++ src/modules/openai/openai-prompt.service.ts
@@ -26,7 +26,6 @@ export class OpenaiPromptService {
             você deve atender os clientes e seguir estritamente as instruções a seguir:
 
             - atender o cliente pelo nome que é {{name}} e agradecer por entrar em contato com a empresa.
-            - informar o código do atendimento ao cliente que é {{orderId}}.
             - informar o cardápio que é: \n${this.company.produtos}
             - o cliente pode pedir uma ou mais opções do cardápio.
             - as opções são vendidas por valor em dinheiro informado pelo cliente, exemplo: 10 reais da opção 1.
@@ -36,6 +35,7 @@ export class OpenaiPromptService {
             - não existe cobrança de valores adicionais de nenhuma natureza.
             - a moeda é somente em Real brasileiro.
             - você não pode oferecer nenhum item que não esteja em nosso cardápio.
+            - informe o código do atendimento ao cliente que é {{orderId}}.
             
             Para confirmar o pedido, solicite ao cliente que digite corretamente o código do seu atendimento e endereço para entrega (nunca solicite o código do atendimento ao cliente sem que antes você tenha previamente informado ao cliente).
             Ao ser confirmado o código do atendimento, encerre o atendimento se despedindo cordialmente.
