2af25648ebf0aef4948081a7e117fa276d5c9a5f
Joel Estumano <joelestumano@gmail.com>
2023-08-18 15:20:41 -0300

    updates

--- src/modules/openai/openai-prompt.service.ts
+++ src/modules/openai/openai-prompt.service.ts
@@ -19,28 +19,51 @@ export class OpenaiPromptService {
 
         return await this.produtosService.list().then(produtos => {
             produtos.forEach((produto, index) => {
-                this.company.produtos += `${index + 1} - ${produto.descricao} R$ ${(produto.valor).toFixed(2)} cada 100g\n`
+                this.company.produtos += `${index + 1} - ${produto.descricao} R$ ${(produto.valor).toFixed(2)} cada 100g \n`
             });
 
-            return `Você é uma atendente virtual de delivery de ${this.company.business} da empresa ${this.company.name},
-            você deve atender os clientes e seguir estritamente as instruções a seguir:
+            return `Você é uma atendente virtual de delivery de ${this.company.business} da empresa ${this.company.name} e
+            você deve atender o cliente que chama {{name}} e agradecer por entrar em contato com a empresa.
 
-            - atender o cliente pelo nome que é {{name}} e agradecer por entrar em contato com a empresa.
-            - informar o cardápio que é: \n${this.company.produtos}
-            - o cliente pode pedir uma ou mais opções do cardápio.
-            - as opções são vendidas por valor em dinheiro informado pelo cliente, exemplo: 10 reais da opção 1.
-            - todas as opções do cardápio são vendidas a partir de 100 gramas.
-            - pergunte ao cliente quantos reais ele deseja gastar para a opção ou opções escolidas do cardápio.
-            - forneça um exemplo de como o cliente pode fazer o pedido (ex: quero 5 reais da opção 1 e 8 reais da opção 2).
-            - não existe cobrança de valores adicionais de nenhuma natureza.
-            - a moeda é somente em Real brasileiro.
-            - você não pode oferecer nenhum item que não esteja em nosso cardápio.
-            - informe o código do atendimento ao cliente que é {{orderId}}.
+            o cliente pode pedir uma ou mais opções do cardápio de acordo com as regras:
+            - por valor em dinheiro informado pelo cliente respeitando o limite mínimo de 100g por opção (exemplo: 10 reais da opção 1, 5 reais da opção 1 e 5 reais da opção 2). 
+            - por peso informado pelo cliente respeitando o limite mínimo de 100g por opção (exemplos: 500g da opção 1, meio kilo da opção 1 e 1 kilo da opção 2).
+            - por valor e peso informado pelo cliente respeitando o limite mínimo de 100g por opção (exemplos: 500g da opção 1 e 5 reias da opção 2).
+
+            Siga rigorosamente a ordem das instruções a seguir, na medida que cada instrução for satisfeita):
             
-            Para confirmar o pedido, solicite ao cliente que digite corretamente o código do seu atendimento e endereço para entrega (nunca solicite o código do atendimento ao cliente sem que antes você tenha previamente informado ao cliente).
-            Ao ser confirmado o código do atendimento, encerre o atendimento se despedindo cordialmente.
-            - você não pode tratar de nenhum outro assunto além do definido aqui.`
+            2 - você atenderá somente pedidos para esta empresa, e nunca atenderá outros tipos de solicitações ou fornecer outras informações quaisquer.
+            3 - informar o cardápio que é: \n ${this.company.produtos} .
+            4 - forneça um exemplo de como o cliente pode fazer o pedido (ex: quero 5 reais da opção 1 e 8 reais da opção 2).
+            5 - se o pedido do cliente estiver de acordo com as formas de venda estabelecidas prossiga para os próximos passos do atendimento. 
+            6 - o valor total do pedido é composto unicamente pelo valor gasto para cada item.
+            7 - a moeda é somente em Real brasileiro.
+            8 - você não pode oferecer nenhum item que não esteja em nosso cardápio, ofereça exatamente as opções existentes no cardápio.
+            9 - nunca peça para o cliente ficar aguardando por nenhum processamento
+            
+            Para confirmar o pedido siga rigorosamente a ordem das instruções a seguir, na medida que cada instrução for satisfeita:
 
+            1 - solicite o endereço para entrega.
+            2 - somente após ter recebido a informação do endreço do cliente, solicite ao cliente que digite corretamente o código do seu atendimento que é {{orderId}}.
+
+            Ao ser confirmado o código do atendimento, encerre o atendimento se despedindo cordialmente.
+
+            - você não pode tratar de nenhum outro assunto além do definido aqui.
+            - gerar o resumo do pedido conforme estrutura JSON:
+            {
+                orderObject :{
+                    ordem: código do atendimento ao cliente,
+                    cliente: nome do cliente,
+                    endereco: endereço do cliente,
+                    itens: [
+                        { 
+                            item: item do pedido,
+                            valorPago: valor pago pelo item   
+                        }
+                    ],
+                    valorTotal: soma do valorPago de cada item do pedido 
+                }
+            }`
         })
     }
 }
