b6f2aba5a1c7a7a9a70dccebdbb3f4164edabbc5
Joel Estumano <joelestumano@gmail.com>
2023-08-28 14:32:35 -0300

    updates version

--- src/common/configs/company.config.ts
+++ src/common/configs/company.config.ts
@@ -3,4 +3,6 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('company', () => ({
     business: `${process.env.COMPANY_BUSINESS}`,
     name: `${process.env.COMPANY_NAME}`,
+    address: `${process.env.COMPANY_ADDRESS}`,
+    contact: `${process.env.COMPANY_CONTACT}`
 }));
--- src/modules/openai/openai-prompt.service.ts
+++ src/modules/openai/openai-prompt.service.ts
@@ -9,7 +9,9 @@ export class OpenaiPromptService {
     private company = {
         business: this.configService.get<string>('company.business') ? this.configService.get<string>('company.business') : EBusiness.food,
         name: this.configService.get<string>('company.name') ? this.configService.get<string>('company.name') : 'Default Company',
-        produtos: ''
+        produtos: '',
+        address: this.configService.get<string>('company.address') ? this.configService.get<string>('company.address') : 'Default Company address',
+        contact: this.configService.get<string>('company.contact') ? this.configService.get<string>('company.contact') : 'Default Company contact'
     };
 
     constructor(private readonly configService: ConfigService,
@@ -22,14 +24,18 @@ export class OpenaiPromptService {
                 this.company.produtos += `${index + 1} - ${produto.descricao} R$ ${(produto.valor).toFixed(2)} cada 100g \n`
             });
 
-            return `Você é a atendente virtual de delivery de ${this.company.business} da empresa ${this.company.name} e
+            return `Você é a atendente virtual de delivery de ${this.company.business} da empresa ${this.company.name}
+            dados da empresa:
+            - endereço: ${this.company.address}
+            - contato: ${this.company.contact}
+
             você deve atender o cliente que chama {{name}} e agradecer por entrar em contato com a empresa.
             
             O cliente pode pedir uma ou mais opções do cardápio de acordo com as regras:
             - por valor em dinheiro informado pelo cliente (ex: 10 reais da opção 1, 5 reais da opção 1 e 5 reais da opção 2).
             - por peso informado pelo cliente (ex: 500g da opção 1, meio kilo da opção 1 e 1 kilo da opção 2).
             - por valor e peso informado pelo cliente (ex: 500g da opção 1 e 5 reias da opção 2).
-            - por valor e descrição informado pelo cliente (ex: 500g de frango, 5 reias de peixe).
+            - por descrição informada pelo cliente (ex: 500g de frango, 5 reias de peixe).
             - todas as opções são vendidas somente a partir de 100 gramas.
             - forneça exemplos de como o cliente pode fazer o pedido
             
@@ -44,8 +50,8 @@ export class OpenaiPromptService {
             7 - nunca peça para o cliente ficar aguardando por nenhum processamento.
             
             Para confirmar o pedido siga rigorosamente a ordem das instruções a seguir, na medida que cada instrução for satisfeita:
-            1 - solicite o endereço para entrega.
-            2 - somente após ter recebido a informação do endreço do cliente, solicite ao cliente que digite corretamente o código do seu atendimento que é {{orderId}}.
+            1 - solicite o endereço para entrega ou horário válido para retirada.
+            2 - somente após ter recebido a informação do endreço do cliente ou horário válido para retirada, solicite ao cliente que digite corretamente o código do seu atendimento que é {{orderId}}.
             Ao ser confirmado o código do atendimento, encerre o atendimento se despedindo cordialmente.
             - você não pode tratar de nenhum outro assunto além do definido aqui.
             - gerar o resumo do pedido conforme estrutura JSON:
