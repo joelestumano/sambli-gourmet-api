f6f1a1a87e92c6b5baaa0d48573e080334229ef9
Joel Estumano <joelestumano@gmail.com>
2023-09-06 11:46:59 -0300

    produtos-changed

--- src/modules/produtos/listeners/produto-updated.listener.ts
+++ src/modules/produtos/listeners/produto-updated.listener.ts
@@ -3,8 +3,8 @@ import { OnEvent } from '@nestjs/event-emitter';
 import { CustomEvent } from '../../../common/events/custom-event.event';
 
 @Injectable()
-export class ProdutoUpdatedListener {
-  private readonly logger = new Logger(ProdutoUpdatedListener.name);
+export class ProdutosChangedListener {
+  private readonly logger = new Logger(ProdutosChangedListener.name);
 
   @OnEvent('produtos-changed')
   handleProdutosChangedEvent(event: CustomEvent) {
--- src/modules/produtos/produtos.module.ts
+++ src/modules/produtos/produtos.module.ts
@@ -3,10 +3,11 @@ import { ProdutosController } from './produtos.controller';
 import { ProdutosService } from './produtos.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Produto, ProdutoSchema } from './entities/produto.entity';
+import { ProdutosChangedListener } from './listeners/produto-updated.listener';
 
 @Module({
   controllers: [ProdutosController],
-  providers: [ProdutosService],
+  providers: [ProdutosService, ProdutosChangedListener],
   imports: [MongooseModule.forFeature([{ name: Produto.name, schema: ProdutoSchema }])],
   exports: [ProdutosService]
 })
