912113838654c17952ff1bab69793ece93b9ac26
Joel Estumano <joelestumano@gmail.com>
2023-10-02 17:44:38 -0300

    api-is-public decorator

--- src/modules/usuario/usuario.controller.ts
+++ src/modules/usuario/usuario.controller.ts
@@ -2,7 +2,6 @@ import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UsuarioCreateDto } from './dtos/usuario-create.dto';
 import { UsuarioService } from './usuario.service';
-import { ApiPublicEndpoint } from '../auth/decorators/api-public-endpoint.decorator';
 
 @Controller('v1/usuario')
 @ApiTags('v1/usuario')
@@ -10,7 +9,6 @@ export class UsuarioController {
 
     constructor(private readonly usuarioService: UsuarioService) { }
 
-    @ApiPublicEndpoint()
     @Post('create')
     @ApiOperation({
         summary: 'registra novo usuário',
