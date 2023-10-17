0f5ae74fb31ef20c8c0e21ee05d076861ebe7912
Joel Estumano <joelestumano@gmail.com>
2023-10-17 16:00:27 -0300

    label taxa

--- src/modules/taxas-e-servicos/dtos/taxas-e-servicos-update.dto.ts
+++ src/modules/taxas-e-servicos/dtos/taxas-e-servicos-update.dto.ts
@@ -1,4 +1,4 @@
 import { PickType } from "@nestjs/swagger";
 import { TaxaServicoCreateDto } from "./taxas-e-servicos-create.dto";
 
-export class TaxaServicoUpdateDto extends PickType(TaxaServicoCreateDto, ['valor'] as const) { } 
+export class TaxaServicoUpdateDto extends PickType(TaxaServicoCreateDto, ['valor', 'label'] as const) { } 
