aca4a8798cf9cb7e117ab68b3ef3c7902c20c8c8
Joel Estumano <joelestumano@gmail.com>
2023-10-24 09:58:10 -0300

    Refatoração módulo taxas

--- src/modules/pedidos/decorators/is-valid-valor-total-constraint.decorator.ts
+++ src/modules/pedidos/decorators/is-valid-valor-total-constraint.decorator.ts
@@ -1,6 +1,6 @@
 import { registerDecorator, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface, ValidationArguments } from 'class-validator';
 import { PedidoCreateDto } from '../dtos/pedido-create.dto';
-import { Logger } from '@nestjs/common';
+import { BadRequestException, Logger } from '@nestjs/common';
 import { PedidoTaxaDto } from '../dtos/taxa-servico.dto';
 import { TaxaRefEnum } from 'src/modules/taxas-e-servicos/entities/taxa.entity';
 
@@ -10,16 +10,33 @@ export class IsValidValorTotalConstraint implements ValidatorConstraintInterface
     private readonly logger = new Logger(IsValidValorTotalConstraint.name);
 
     validate(valorTotal: Number, args: ValidationArguments) {
-
         const dto = args.object as PedidoCreateDto;
+
         const pagamento = dto.pagamento;
 
-        const taxas = ("taxas" in dto) ? dto.taxas.filter((t: PedidoTaxaDto)=> t.referencia !== TaxaRefEnum.ENTREGA).reduce((total, taxa) => {
-            return total + taxa.valor; /** todo */
-        }, 0) : 0;
+        if (dto.isDeliver) {
 
-        const sumaPagamento = Object.values(pagamento).reduce((suma: number, valor: number) => suma + valor, 0);
-        return (sumaPagamento + taxas) === Number(valorTotal);
+            if (!('taxas' in dto)) {
+                throw new BadRequestException(`valor total no pedido para entrega requer informações da taxa de entrega`);
+            }
+
+            const pedidoTaxaEntrega = dto.taxas.find((t: PedidoTaxaDto) => t.referencia === TaxaRefEnum.ENTREGA);
+
+            if (!pedidoTaxaEntrega) {
+                throw new BadRequestException(`taxa de entrega não informada na lista de taxas`);
+            }
+
+            return true
+
+        } else {
+
+            const taxas = ("taxas" in dto) ? dto.taxas.reduce((total, taxa) => {
+                return total + taxa.valor;
+            }, 0) : 0;
+
+            const sumaPagamento = Object.values(pagamento).reduce((suma: number, valor: number) => suma + valor, 0);
+            return (sumaPagamento + taxas) === Number(valorTotal);
+        }
     }
 }
 
