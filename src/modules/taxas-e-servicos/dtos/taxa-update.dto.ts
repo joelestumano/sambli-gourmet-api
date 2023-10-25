d4df7e1ce61cfc2469572b5bfa90dc06516e8010
Joel Estumano <joelestumano@gmail.com>
2023-10-25 11:04:09 -0300

    Deploy ajuste optional taxa

--- src/modules/taxas-e-servicos/dtos/taxa-update.dto.ts
+++ src/modules/taxas-e-servicos/dtos/taxa-update.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty, PickType } from "@nestjs/swagger";
-import { TaxaCreateDto } from "./taxa-create.dto";
-import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+import { IsNumber, IsOptional, IsString } from "class-validator";
 import { Type } from "class-transformer";
 
 export class TaxaUpdateDto { 
